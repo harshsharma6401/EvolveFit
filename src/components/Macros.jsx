24b6e097d54ee99fe1ef4d5722888b3d3a61fdbf
Harsh Sharma <thisis.sharsh@gmail.com>
2022-08-03 00:39:28 +0530

    Tooltip styled

--- src/components/Macros.jsx
+++ src/components/Macros.jsx
@@ -1,57 +1,39 @@
 import React from 'react'
 import { darkTheme } from './Header'
-import { ThemeProvider, LinearProgress, Paper, Tooltip, Typography, createTheme } from '@mui/material'
+import { ThemeProvider, LinearProgress, Paper, Tooltip, Typography,Box } from '@mui/material'
 
 const Macros = () => {
-
-    const styles = () => ({
-        colorPrimary: {
-            backgroundColor: '#00695C',
-        },
-        barColorPrimary: {
-            backgroundColor: '#B2DFDB',
-        }
-    });
-
-    const progressTheme = createTheme({
-        palette: {
-            mode: 'dark',
-            background: {
-                paper: '#1E262F',
-            },
-            text: {
-                primary: '#F45C84',
-                secondary: '#F0C50F',
-            },
-        },
-    });
 
     return (
         <ThemeProvider theme={darkTheme}>
 
-            <Paper sx={{width: 300, py: 1, px: 3, borderRadius: 3 }}>
-
-                <Typography>PROTEIN</Typography>
+            <Paper sx={{width: 200, py: 1, px: 0.5, borderRadius: 3 }}>
+                <Box sx ={{my:1, py:1, px:1,backgroundColor :"#1B222A" ,borderRadius: 3}} >
+                <Typography  display ="inline" sx ={{fontSize :"12px"}}>PROTEIN  </Typography> <Typography display ="inline" sx={{ fontSize :"12px",marginLeft:12}} > {'80g'} </Typography>
                 <LinearProgress sx={{
                     '& .MuiLinearProgress-bar1Determinate': {
                         backgroundColor: '#F45C84',
                     }, height: 10, borderRadius: 10
                 }} variant="determinate" value={20} />
-                <Typography>FATS</Typography>
+                </Box>
+                <Box sx ={{my:1, py:1, px:1,backgroundColor :"#1B222A" ,borderRadius: 3}} >
+                <Typography display ="inline" sx ={{fontSize :"12px"}}>FATS</Typography> <Typography display ="inline" sx={{ fontSize :"12px",marginLeft:15}} > {'80g'} </Typography>
                 <LinearProgress sx={{
                     '& .MuiLinearProgress-bar1Determinate': {
                         backgroundColor: '#03C6FA',
                     }
                     , height: 10, borderRadius: 10
                 }} variant="determinate" value={20} />
-                <Typography>CARBS</Typography>
+                  </Box>
+                  <Box sx ={{my:1, py:1, px:1,backgroundColor :"#1B222A" ,borderRadius: 3}} >
+                <Typography display ="inline" sx ={{fontSize :"12px"}}>CARBS</Typography> <Typography display ="inline" sx={{ fontSize :"12px",marginLeft:13.5}} > {'80g'} </Typography>
                 <LinearProgress sx={{
                     '& .MuiLinearProgress-bar1Determinate': {
                         backgroundColor: '#F0C50F',
                     }
                     , height: 10, borderRadius: 10
                 }} variant="determinate" value={20} />
-
+                  </Box>
 
             </Paper>
 
