505e378aba8fc01db400d7f219ed947cee46f95c
Harsh Sharma <thisis.sharsh@gmail.com>
2022-08-03 06:31:05 +0530

    Fixing for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './components/Pages/HomePage';
-import Nutrition from './components/Pages/nutrition';
-import Workout from './components/Pages/workout';
 import Header from './components/Header';
 
 function App() {
